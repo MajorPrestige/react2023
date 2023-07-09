4fba1bba6e41c34e44e604a7f07b04b394001681
Denys Dashkevych <denysdashkevych@gmail.com>
2023-07-08 23:19:52 -0500

    svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-const title = 'hello world!';
+import { ReactComponent as Logo } from './assets/react.svg';
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      Hello World
+      <img src="/vite.svg" alt="" />
+      <Logo />
+    </div>
+  );
 };
 
 export default App;
